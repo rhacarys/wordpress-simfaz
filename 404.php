6cf1eca5192758d84772384810eb06210188f4e1
Nathaniel Lacerda <nathaniel.lacerda@gmail.com>
2020-11-03 22:48:11 -0300

    Error pages, screenshot and minor css improvments

---
+++ 404.php
@@ -0,0 +1,19 @@
+<?php
+/*
+ * Template Name: Template Principal
+ * description: >-
+  Page template with a huge title and detailed content
+ */
+
+get_header(); ?>
+
+<div class="site-content full-height full-width desktop-flex">
+    <div class="full-height full-width vertical-center center">
+        <div class="error-container">
+            <div class="error-code">404</div>
+            <div class="error-description">Esta página não existe</div>
+        </div>
+    </div>
+</div><!-- site-content -->
+
+<?php get_footer(); ?>
---
+++ 500.php
@@ -0,0 +1,19 @@
+<?php
+/*
+ * Template Name: Template Principal
+ * description: >-
+  Page template with a huge title and detailed content
+ */
+
+get_header(); ?>
+
+<div class="site-content full-height full-width desktop-flex">
+    <div class="full-height full-width vertical-center center">
+        <div class="error-container">
+            <div class="error-code">500</div>
+            <div class="error-description">Desculpe, algo deu errado</div>
+        </div>
+    </div>
+</div><!-- site-content -->
+
+<?php get_footer(); ?>
